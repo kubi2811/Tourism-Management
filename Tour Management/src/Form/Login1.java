9ded23ba16d38aa1de200d0a68e5bc3f9a14acf9
Kaltovia <ranconbonmat@gmail.com>
2022-07-09 17:51:02 +0700

    clone login

--- Tour Management/src/Form/Login1.java
+++ Tour Management/src/Form/Login1.java
@@ -15,12 +15,12 @@ import javax.swing.UnsupportedLookAndFeelException;
  *
  * @author ranco
  */
-public class Login extends javax.swing.JFrame {
+public class Login1 extends javax.swing.JFrame {
 
     /**
      * Creates new form NewJFrame
      */
-    public Login() {
+    public Login1() {
         initComponents();
     }
 
@@ -221,14 +221,21 @@ public class Login extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+//        try{
+//            UIManager.setLookAndFeel("de.javasoft.synthetica.simple2d.SyntheticaSimple2DLookAndFeel");
+//        } catch (IllegalAccessException ex){
+//            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+//        }
         //</editor-fold>
         //</editor-fold>
 //        try{
@@ -241,7 +248,7 @@ public class Login extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Login().setVisible(true);
+                new Login1().setVisible(true);
             }
         });
     }
